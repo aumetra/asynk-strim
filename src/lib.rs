73e9cd47f612998bd053fa22240b8938366e5372
Aumetra Weisman <aumetra@cryptolab.net>
2024-10-08 10:31:50 +0200

    Add more seriously named function, add docs on usage

--- src/lib.rs
+++ src/lib.rs
@@ -40,11 +40,48 @@ where
 }
 
 /// Create a new stream
+///
+/// # Example
+///
+/// Let's yield some lyrics (Song: "Verdächtig" by Systemabsturz):
+/// 
+/// ```
+/// # use futures_lite::StreamExt;
+/// # use std::pin::pin;
+/// # futures_lite::future::block_on(async {
+/// let stream = asynk_strim::create_stream(|mut yielder| async move {
+///    yielder.yield_item("Fahr den Imsi-Catcher hoch").await;
+///    yielder.yield_item("Mach das Richtmikro an").await;
+///    yielder.yield_item("Bring Alexa auf den Markt").await;
+///    yielder.yield_item("Zapf den Netzknoten an").await;
+///    yielder.yield_item("Fahr den Ü-Wagen vor").await;
+///    yielder.yield_item("Kauf den Staatstrojaner ein").await;
+///    yielder.yield_item("Fake die Exit-Nodes bei Tor").await;
+///    yielder.yield_item("Ihr wollt doch alle sicher sein").await;
+/// });
+///
+/// let mut stream = pin!(stream);
+/// while let Some(item) = stream.next().await {
+///    println!("{item}");
+/// }
+/// # });
+#[inline]
+pub fn create_stream<F, Item, Fut>(func: F) -> impl Stream<Item = Item>
+where
+    F: FnOnce(Yielder<Item>) -> Fut,
+    Fut: Future<Output = ()>,
+{
+    crate::stream::init(func)
+}
+
+/// Jokey alias for `create_stream`
+///
+/// For more elaborate documentation, see [`create_stream`]
 #[inline]
 pub fn strim_fn<F, Item, Fut>(func: F) -> impl Stream<Item = Item>
 where
     F: FnOnce(Yielder<Item>) -> Fut,
     Fut: Future<Output = ()>,
 {
-    crate::stream::init(func)
+    create_stream(func)
 }
